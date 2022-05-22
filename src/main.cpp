469c8b63e679807e1fd3b589bd798644172509d6
andrew <andy816816@gmail.com>
2022-05-22 02:03:36 -0700

    set all pin modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,32 @@
 #include <Arduino.h>
 
-int incermentAddr1 = 0;
-int incermentAddr2 = 1;
-int reset = 13;
-int chipEnable = 2;
-int outEnable = 3;
-int writeEnable = 4;
+int incrementAddress1 = 0;
+int incrementAddress2 = 1;
+int resetAddress = 13;
+int romChipEnable = 2;
+int romOutEnable = 3;
+int romWriteEnable = 4;
 
-int dataIO[8] = {5, 6, 7, 8, 9, 10, 11};
+int romIO[8] = {5, 6, 7, 8, 9, 10, 11};
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(incermentAddr1, OUTPUT);
-  pinMode(reset, OUTPUT);
-  digitalWrite(reset, LOW);
+  pinMode(incrementAddress1, OUTPUT);
+  pinMode(incrementAddress2, OUTPUT);
+  pinMode(resetAddress, OUTPUT);
+  pinMode(romChipEnable, OUTPUT);
+  pinMode(romOutEnable, OUTPUT);
+  pinMode(romWriteEnable, OUTPUT);
+
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(romIO[i], OUTPUT);
+  }
+
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(incermentAddr1, HIGH);
-  delay(1);
-  digitalWrite(incermentAddr1, LOW);
-  delay(1);
 }
