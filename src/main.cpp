cf3c85a48d6eb4d49c8fca20ee55adc2624ddabf
andy9879 <andy816816@gmail.com>
2022-05-23 02:09:00 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,55 @@
 #include <Arduino.h>
 
-int incrementAddress1 = A2;
-int incrementAddress2 = A5;
-int resetAddress = 13;
-int romChipEnable = 2;
-int romOutEnable = 3;
-int romWriteEnable = 4;
+uint8_t incrementAddress1 = A2;
+uint8_t incrementAddress2 = A5;
+uint8_t resetAddress = 13;
+uint8_t romChipEnable = 2;
+uint8_t romOutEnable = 3;
+uint8_t romWriteEnable = 4;
 
-int romIO[8] = {5, 6, 7, 8, 9, 10, 11, 12};
+uint8_t romIO[8] = {5, 6, 7, 8, 9, 10, 11, 12};
 
-void writeToRam()
+void writeToRam(uint8_t data)
 {
+
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    pinMode(romIO[i], OUTPUT);
+  }
+
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.println(data % 2);
+    if (data % 2 == 0)
+    {
+      digitalWrite(romIO[i], HIGH);
+    }
+    else
+    {
+      digitalWrite(romIO[i], LOW);
+    }
+    data = data >> 1;
+  }
+
+  delay(5000);
+  digitalWrite(romWriteEnable, LOW);
+  delay(5000);
+  digitalWrite(romWriteEnable, HIGH);
+
+  for (uint8_t i = 0; i < 8; i++)
+  {
+
+    digitalWrite(romIO[i], LOW);
+  }
 }
 
 void setup()
 {
   Serial.begin(9600);
   delay(3000);
+  uint8_t test = 8;
+  test = test >> 2;
+  Serial.println(test);
   Serial.println("Starting....");
   // put your setup code here, to run once:
   pinMode(incrementAddress1, OUTPUT);
@@ -36,33 +69,21 @@ void setup()
   digitalWrite(romWriteEnable, HIGH);
   delay(100);
 
-  digitalWrite(romIO[0], HIGH);
-  digitalWrite(romIO[1], LOW);
-  digitalWrite(romIO[2], LOW);
-  digitalWrite(romIO[3], LOW);
-  digitalWrite(romIO[4], LOW);
-  digitalWrite(romIO[5], LOW);
-  digitalWrite(romIO[6], LOW);
-  digitalWrite(romIO[7], LOW);
-  delay(100);
-  digitalWrite(romWriteEnable, LOW);
-  delay(100);
-  digitalWrite(romWriteEnable, HIGH);
+  writeToRam(0b00000000);
 
-  for (int i = 0; i < 8; i++)
+  for (uint8_t i = 0; i < 8; i++)
   {
-    digitalWrite(romIO[i], LOW);
     pinMode(romIO[i], INPUT);
   }
+
   digitalWrite(romOutEnable, LOW);
-  delay(5000);
 }
 
 void loop()
 {
 
   delay(10);
-  for (int i = 0; i < 8; i++)
+  for (uint8_t i = 0; i < 8; i++)
   {
     Serial.print(digitalRead(romIO[i]));
   }
